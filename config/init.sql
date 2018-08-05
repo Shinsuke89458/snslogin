bcd9e363747e56db68a2043768eb9704ff3bfb23
Shinsuke89458 <uriuf55@yahoo.co.jp>
2018-08-05 13:01:40 +0900

    Add files via upload

---
+++ config/init.sql
@@ -0,0 +1,15 @@
+create database snsdb;
+
+grant all on snsdb.* to dbuser@localhost identified by 'shin0622';
+
+use snsdb
+
+create table users (
+  id int not null auto_increment primary key,
+  email varchar(255) unique,
+  password varchar(255),
+  created datetime,
+  modified datetime
+);
+
+desc users;
